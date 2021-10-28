189f317fbed0e87aa6f01425ea1169c3fdf99d40
Luis Ramírez Villanueva <mat_luis16@hotmail.com>
2021-10-27 22:34:58 -0500

    Agregue el estado inicial de la app y las imagenes para el proyecto

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    products: [
+      { name: 'Tomato', price: 15, img: '/assets/products/tomato.jpg' },
+      { name: 'Peas', price: 5, img: '/assets/products/peas.jpg' },
+      { name: 'Lettuce', price: 25, img: '/assets/products/lettuce.jpg' },
+    ]
+  }
   render() {
     return (
       <div>
