589db47fc27180ebfaaf18aa765d0d1b858e108c
Luis Ramírez Villanueva <mat_luis16@hotmail.com>
2021-10-27 23:21:14 -0500

    Añadi el boton agregar al carrito

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Button from './Button';
 
 const styles = {
   product: {
@@ -15,12 +16,15 @@ const styles = {
 
 class Product extends Component {
   render() {
-    const {product} = this.props;
+    const {product, agregarAlCarro} = this.props;
     return (
       <div style={styles.product}>
         <img style={styles.img} alt={product.img} src={product.img} />
         <h3>{product.name}</h3>
         <p>{product.price}</p>
+        <Button onClick={() => agregarAlCarro(product)}>
+          Agregar al carrito
+        </Button>
       </div>
     )
   }
