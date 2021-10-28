2955a99f7482784d2a3dd015043b64214e7df7c3
Luis Ramírez Villanueva <mat_luis16@hotmail.com>
2021-10-27 23:10:38 -0500

    Agregue inline style a los componentes products y product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,24 @@
 import { Component } from 'react';
 
+const styles = {
+  product: {
+    border: 'solid 1px #EEE',
+    boxShadow: '0 5px 5px rgb(0, 0, 0, 0.1)',
+    with: '30%',
+    padding: '10px 15px',
+    borderRadius: '5px',
+  },
+  img: {
+    width: '100%',
+  }
+}
+
 class Product extends Component {
   render() {
     const {product} = this.props;
     return (
-      <div>
-        <img alt={product.img} src={product.img} />
+      <div style={styles.product}>
+        <img style={styles.img} alt={product.img} src={product.img} />
         <h3>{product.name}</h3>
         <p>{product.price}</p>
       </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,19 @@
 import { Component } from 'react';
 import Product from './Product';
 
+const styles = {
+  products: {
+    display: 'flex',
+    flexDirecton: 'row',
+    justifyContent: 'space-between',
+  }
+}
+
 class Products extends Component {
   render() {
     const { products, agregarAlCarro } = this.props;
-
     return (
-      <div>
+      <div style={styles.products}>
         {products.map(product =>
           <Product
             agregarAlCarro={agregarAlCarro}
